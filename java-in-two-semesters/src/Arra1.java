806f03ecc86bc77c4fa0175f829aa495591cad18
usuario <usuario@usuario-PC>
2020-05-11 12:39:48 -0500

    Mi First Commit
    Initializing Repository

---
+++ java-in-two-semesters/src/Arra1.java
@@ -0,0 +1,18 @@
+
+public class Arra1 {
+
+	public static void main(String[] args) {
+		// 
+
+		double[] temperatura = new double[7];
+		
+		double[] temp = {1,2.3,3.5,4,5,6};
+		double temp2[] = {1,2.3,3.5,4,5,6};
+		System.out.println(String.valueOf(temp2[2]));
+		System.out.println(String.valueOf(temp.length));
+		System.out.println(String.valueOf(temp2.length));
+		System.out.println(String.valueOf(temperatura.length));
+		
+	}
+
+}
